aa6ab96789a3d3d9da6c9d2e6e5fc55b54e6c8d2
도준회 <brandjunhoe@kai-i.com>
2020-08-03 17:09:04 +0900

    첫 커밋

---
+++ contentsService/src/main/java/com/brandjunhoe/contents/ContentsApplication.java
@@ -0,0 +1,13 @@
+package com.brandjunhoe.contents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentsApplication.class, args);
+    }
+
+}
---
+++ memberService/src/test/java/com/brandjunhoe/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandjunhoe.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
